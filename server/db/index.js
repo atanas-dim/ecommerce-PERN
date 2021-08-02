29df4adb1008fd726af1195b600fc16b27123e18
atanasdim <hi.atanasdim@gmail.com>
2021-08-02 19:08:26 +0100

    Swagger updated

--- server/db/index.js
+++ server/db/index.js
@@ -1,13 +1,5 @@
 const { Pool } = require("pg");
 
-// const devConfig = {
-//   user: process.env.PGUSER,
-//   host: process.env.PGHOST,
-//   database: process.env.PGDATABASE,
-//   password: process.env.PGPASSWORD,
-//   port: process.env.PGPORT,
-// };
-
 const devConfig = `postgresql://${process.env.PGUSER}:${process.env.PGPASSWORD}@${process.env.PGHOST}:${process.env.PGPORT}/${process.env.PGDATABASE}`;
 
 const proConfig = process.env.DATABASE_URL; //heroku addons
