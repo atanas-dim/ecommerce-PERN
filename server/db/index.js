a1e5d3ff1efb342104c07b649654f69a473312c7
atanasdim <hi.atanasdim@gmail.com>
2021-08-02 19:02:13 +0100

    db pool ssl update

--- server/db/index.js
+++ server/db/index.js
@@ -1,19 +1,23 @@
 const { Pool } = require("pg");
 
-const devConfig = {
-  user: process.env.PGUSER,
-  host: process.env.PGHOST,
-  database: process.env.PGDATABASE,
-  password: process.env.PGPASSWORD,
-  port: process.env.PGPORT,
-};
+// const devConfig = {
+//   user: process.env.PGUSER,
+//   host: process.env.PGHOST,
+//   database: process.env.PGDATABASE,
+//   password: process.env.PGPASSWORD,
+//   port: process.env.PGPORT,
+// };
 
-const proConfig = {
-  connectionString: process.env.DATABASE_URL,
-};
+const devConfig = `postgresql://${process.env.PGUSER}:${process.env.PGPASSWORD}@${process.env.PGHOST}:${process.env.PGPORT}/${process.env.PGDATABASE}`;
 
-const pool = new Pool(
-  process.env.NODE_ENV === "production" ? proConfig : devConfig
-);
+const proConfig = process.env.DATABASE_URL; //heroku addons
+
+const pool = new Pool({
+  connectionString:
+    process.env.NODE_ENV === "production" ? proConfig : devConfig,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 
 module.exports = pool;
